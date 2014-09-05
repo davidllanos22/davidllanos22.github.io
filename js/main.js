ecc3c41304cb2159371ea56501f9235d13e255d4
David Llanos López <davidllanos22@gmail.com>
2014-09-05 02:02:34 +0200

    added tags colors and quick-fixed footer

--- js/main.js
+++ js/main.js
@@ -9,11 +9,31 @@ $(document).ready(function(){
 				$(xml).find("post").each(function(){
 					var title = $(this).find("title").text();
 					var content = $(this).find("content").text();
-
+					var date = $(this).find("date").text();
+					
 					output +='<div class="post">';
 					output +='<h2 class="post-title"><a href="#">'+title+'</a></h2>';
+					output +='<p class="post-date">'+date+'</p>';
 					output +='<p class="post-content">'+content+'</p>';
-					output += '</div>';
+					output +='<ul class="post-tags">';
+					
+					var tags = $(this).find("tags");
+
+					$(tags).find("tag").each(function(){
+						var color = "gray";
+						if($(this).text()=="gamedev"){
+							color = "purple" 
+						}else if($(this).text()=="webdev"){
+							color = "blue" 
+						}else if($(this).text()=="pixelart"){
+							color = "red" 
+						}else if($(this).text()=="tutorial"){
+							color = "green" 
+						}
+						output +='<li class="'+color+'">'+$(this).text()+'</li>';
+					});
+					
+					output += '</ul></div>';
 				});
 
 				$("#content").append(output);
